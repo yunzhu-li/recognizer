873b6d757b317e155020171bac4b2617f077e101
Yunzhu Li <contact@yunzhu.li>
2018-04-14 17:50:47 -0400

    Add missing package (#7)

--- image_classifier/packer/provision.sh
+++ image_classifier/packer/provision.sh
@@ -7,6 +7,7 @@ sudo apt full-upgrade -y
 # Install CUDA apt repo
 # https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1604
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+sudo apt install -y gnupg-curl # HTTPS support for gnupg
 sudo sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo dpkg -i cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
 sudo apt update
